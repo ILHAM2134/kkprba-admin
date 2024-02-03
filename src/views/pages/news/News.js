13259606ddc432f49e260910b97a0949256aa0ea
farelamo <farizfahmi@synapsis.id>
2024-02-03 11:39:44 +0700

    monyekksss ngntod

--- src/views/pages/news/News.js
+++ src/views/pages/news/News.js
@@ -98,7 +98,7 @@ const News = () => {
     if (tag === 'fetch') {
       setLoadingPage(true)
 
-      axios('/api/blog')
+      axios('/blog')
         .then((res) => {
           const data = res?.data?.data?.map((item) => ({
             key: item?.id,
